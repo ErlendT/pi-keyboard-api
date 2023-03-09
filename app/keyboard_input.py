28da6bfe7c6fc669afbb308ca4948c1a65d423e1
ErlendT <32453298+ErlendT@users.noreply.github.com>
2023-03-09 18:41:04 +0100

    added return error for debugging

--- app/keyboard_input.py
+++ app/keyboard_input.py
@@ -1,16 +1,18 @@
 from app.hid_map import HID_MAP
 
 NULL_CHAR = chr(0)
-RELEASE_CHAR = NULL_CHAR*8
+RELEASE_CHARS = NULL_CHAR*8
 
 def send_string_as_keystrokes(data: str):
+    # Release keys
+    send_keystrokes(RELEASE_CHARS)
     for char in [*data]:
         if char.isupper():
             send_keystrokes(HID_MAP["KEY_MOD_RSHIFT"] + NULL_CHAR + HID_MAP[f"KEY_{char.upper()}"] +  NULL_CHAR*5)
         else:
             send_keystrokes(NULL_CHAR*2 + HID_MAP[f"KEY_{char.upper()}"] +  NULL_CHAR*5)
         # Release keys
-        send_keystrokes(RELEASE_CHAR)
+        send_keystrokes(RELEASE_CHARS)
 
 
 def send_keystrokes(input):
--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from app.models.keyboard import KeyboardInputModel
 from app.keyboard_input import send_string_as_keystrokes
 
 app = FastAPI()
 
-@app.post("/inputs", status_code=202)
+@app.post("/inputs")
 def insert_input(keyboard_input: KeyboardInputModel):
-    send_string_as_keystrokes(keyboard_input.input)
+    try:
+        send_string_as_keystrokes(keyboard_input.input)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail={"error": str(e)})
     return keyboard_input
 
 @app.get("/")
